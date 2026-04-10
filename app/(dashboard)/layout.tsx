b4c1b365221e10d262387815347efca7a4addf35
ZAHIDHOSEN <zahidhosen203@gmail.com>
2026-04-10 22:45:56 +0600

    change design of dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,14 +6,20 @@ import React from 'react'
 export default function DashboardLayout({children}:{children:React.ReactNode}) {
   return (
       
-         <div className='flex gap-5'>
-        <div>
+      <div className='flex h-screen overflow-hidden bg-gray-50 gap-5'>
+        <div className='hidden md:flex flex-col w-64 bg-white border-r'>
          <Sidebar></Sidebar>
         </div>
+        {/* main content */}
+      <main className='flex-1 flex flex-col min-w-0 overflow-hidden'>
+        <div className="flex-1 overflow-y-auto p-4 md:p-8">
         <div className='max-w-7xl w-full mx-auto my-5'>
-       {children}
+          {children}
+       </div>
+        </div>
+       </main>
            
-      </div>
+     
   
     </div>
    
